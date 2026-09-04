e8bf064ddf70500afe6a0badc148b3b05617e1cb
stevanu <ivanstevanu@gmail.com>
2026-09-04 22:48:15 +0700

    add icon layanan

--- src/data/services.jsx
+++ src/data/services.jsx
@@ -1,26 +1,32 @@
-import { BadgeCheck, Sparkles, Wrench } from "lucide-react";
+import {
+  BadgeCheck,
+  WandSparkles,
+  BroomSparkles,
+  SportShoe,
+  Zap,
+} from "lucide-react";
 
 export const services = [
   {
-    icon: Sparkles,
+    icon: Zap,
     name: "Fast Clean",
     duration: "2 - 3 hari",
     price: "Rp30.000",
   },
   {
-    icon: Sparkles,
+    icon: SportShoe,
     name: "Deep Clean",
     duration: "2 - 3 hari",
     price: "Rp35.000",
   },
   {
-    icon: Wrench,
+    icon: BroomSparkles,
     name: "Hard Clean",
     duration: "2 - 3 hari",
     price: "Rp40.000",
   },
   {
-    icon: Sparkles,
+    icon: WandSparkles,
     name: "Kids Shoes Clean",
     duration: "2 - 3 hari",
     price: "Rp25.000",
